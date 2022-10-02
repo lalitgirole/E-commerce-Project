281cad82ecab972ee2f2f0a3b2f6902f25d3cc41
Lalit Manohar Girole <lalitgirole@gmail.com>
2022-10-02 19:00:59 +0530

    deleted project

--- EcomSellerApp/src/main/java/cdac/ecom/seller/repository/AddressRepo.java
+++
@@ -1,9 +0,0 @@
-package cdac.ecom.seller.repository;
-
-import cdac.ecom.seller.entity.AddressMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepo  extends JpaRepository<AddressMaster,Long> {
-}
--- EcomSellerApp/src/main/java/cdac/ecom/seller/repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package cdac.ecom.seller.repository;
-
-import cdac.ecom.seller.entity.ProductMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo  extends JpaRepository<ProductMaster,Long> {
-}
